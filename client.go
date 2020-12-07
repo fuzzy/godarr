f174367e055f42804c8a4d17a97d678e787eec9e
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2020-12-06 21:42:16 -0800

    added doRequest method to api object, slightly modified from the original liilac/sonarr source

--- client.go
+++ client.go
@@ -1,7 +1,11 @@
 package godarr
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
 	"strings"
@@ -36,6 +40,58 @@ func (api *ApiClient) connSetup() error {
 	api.Address = addressUrl
 
 	api.Client = http.DefaultClient
+	return nil
+}
+
+func (api *ApiClient) doRequest(action, path string, params map[string]string, reqData, resData interface{}) error {
+	lookupUrl := *api.Address
+	parameters := url.Values{}
+
+	if params != nil {
+		for k, v := range params {
+			parameters.Add(k, v)
+		}
+	}
+
+	lookupUrl.RawQuery = parameters.Encode()
+	lookupUrl.Path += path
+	jsonValue, err := json.Marshal(reqData)
+
+	if err != nil {
+		return err
+	}
+
+	req, err := http.NewRequest(action, lookupUrl.String(), bytes.NewBuffer(jsonValue))
+
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Add("X-Api-Key", api.apiKey)
+	response, err := api.Client.Do(req)
+
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode < 200 || response.StatusCode >= 300 {
+		bodyBytes, err := ioutil.ReadAll(response.Body)
+
+		if err == nil {
+			fmt.Printf("Failing (%v) call returned:\n%v", response.StatusCode, string(bodyBytes))
+		}
+
+		return errors.New(fmt.Sprintf("Status code %v", response.StatusCode))
+	}
+
+	body, err := ioutil.ReadAll(response.Body)
+	err = json.NewDecoder(bytes.NewBuffer(body)).Decode(resData)
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
