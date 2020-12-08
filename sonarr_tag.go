b129797bc319feb3f063e4d4b668a87a029eb998
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2020-12-07 22:19:02 -0800

    tag api stubbs

--- sonarr_tag.go
+++ sonarr_tag.go
@@ -1,6 +1,23 @@
-package godarr
+package main
 
 type Tag struct {
 	Label string `json:"label"`
 	ID    int    `json:"id"`
+}
+
+func (sc *SonarrClient) GetTags() ([]*Tag, error) {
+	retv := []*Tag{}
+	return retv, nil
+}
+
+func (sc *SonarrClient) GetTag(tag int) (*Tag, error) {
+	return &Tag{}, nil
+}
+
+func (sc *SonarrClient) NewTag(label string) (*Tag, error) {
+	return &Tag{}, nil
+}
+
+func (tag *Tag) UpdateTag(label string) (*Tag, error) {
+	return &Tag{}, nil
 }
